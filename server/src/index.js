c0432df2628b67aebe1d62ba84a8af348e955c5c
Siddhant <sidsaiba2703@gmail.com>
2024-08-13 00:11:34 +0530

    complete3

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,10 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors('*'));
+app.use(cors({
+  origin: true,
+  credentials : true,
+}));
 
 //routes
 app.use('/api/post',PostRoutes);
