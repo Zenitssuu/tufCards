9275f96044b55bef558dcd9b1618d8ffa4437089
Siddhant <sidsaiba2703@gmail.com>
2024-08-13 00:04:38 +0530

    complete

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -6,6 +6,7 @@ export const connectDB = () => {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        port : process.env.PORT,
     });
     
     connection.connect(function(err){
