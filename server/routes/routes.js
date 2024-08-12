d2465d0f1f3ba54687527ed547758a99751eba1e
Siddhant <sidsaiba2703@gmail.com>
2024-08-13 00:26:36 +0530

    complete4

---
+++ server/routes/routes.js
@@ -0,0 +1,5 @@
+import PostRoutes from "./postRoutes.js"
+import userRoutes from "./userRouter.js"
+
+
+export {PostRoutes,userRoutes}
--- server/src/routes/routes.js
+++
@@ -1,5 +0,0 @@
-import PostRoutes from "../routes/postRoutes.js"
-import userRoutes from "../routes/userRouter.js"
-
-
-export {PostRoutes,userRoutes}
